0b9064491825c26224c28f2f8bdafe3c6d1ba138
Anastasiiasyvak <asyvak@kse.org.ua>
2023-10-28 00:01:59 +0300

    eight assignment folder + changed folder structor seventh h

---
+++ seventh_homework/feature/main.py
@@ -0,0 +1,90 @@
+from fastapi import FastAPI
+from datetime import datetime
+import requests
+import uvicorn
+
+
+app = FastAPI()
+
+def fetch_user_data(offset):
+    url = f'https://sef.podkolzin.consulting/api/users/lastSeen?offset={offset}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data.get('data', [])
+    else:
+        return []
+
+@app.get("/api/stats/user/total")
+async def get_total_online_time(user_id: str):
+    total_time = calculate_total_online_time(user_id)
+
+    print(f"Total time for user {user_id}: {total_time} seconds")
+
+    return {"totalTime": total_time}
+
+@app.get("/api/stats/user/average")
+async def get_average_online_time(user_id: str):
+    weekly_average, daily_average = calculate_average_online_time(user_id)
+
+    print(f"Weekly average for user {user_id}: {weekly_average} seconds")
+    print(f"Daily average for user {user_id}: {daily_average} seconds")
+
+    return {"weeklyAverage": weekly_average, "dailyAverage": daily_average}
+
+def calculate_total_online_time(user_id: str):
+    user_data = fetch_user_data(0)
+
+    total_time = 0
+
+    for user in user_data:
+        if user.get('userId') == user_id:
+            last_seen_date = user.get('lastSeenDate')
+            if last_seen_date:
+                last_seen_datetime = datetime.fromisoformat(last_seen_date[:-1])
+                current_datetime = datetime.now()
+                time_online = current_datetime - last_seen_datetime
+                total_time = time_online.total_seconds()
+                break
+
+    return total_time
+
+def calculate_average_online_time(user_id: str):
+    user_data = fetch_user_data(0)
+    daily_total_time = 0
+    weekly_total_time = 0
+    days_count = 0
+    weeks_count = 0
+    current_date = datetime.now()
+
+    for user in user_data:
+        if user.get('userId') == user_id:
+            last_seen_date = user.get('lastSeenDate')
+            if last_seen_date:
+                last_seen_datetime = datetime.fromisoformat(last_seen_date[:-1])
+                time_online = current_date - last_seen_datetime
+                total_time = time_online.total_seconds()
+
+                if time_online.days == 0:
+                    daily_total_time += total_time
+                    days_count += 1
+
+                if time_online.days <= 7:
+                    weekly_total_time += total_time
+                    weeks_count += 1
+
+    daily_average = daily_total_time / days_count if days_count > 0 else 0
+    weekly_average = weekly_total_time / weeks_count if weeks_count > 0 else 0
+
+    return weekly_average, daily_average
+
+@app.get("/api/report/{report_name}")
+async def get_report(report_name: str, from_date: str, to_date: str):
+    users_data = fetch_user_data(0)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="209.97.181.107", port=8000)
+
---
+++ seventh_homework/tests/integration_test.py
@@ -0,0 +1,33 @@
+import unittest
+from fastapi.testclient import TestClient
+from seventh_homework.feature.main import app
+
+class TestIntegrationAPI(unittest.TestCase):
+    def test_get_total_online_time(self):
+        client = TestClient(app)
+        user_id = "df60f3f8-0e13-b64a-76b5-863bae54478e"
+        response = client.get(f"/api/stats/user/total?user_id={user_id}")
+        self.assertEqual(response.status_code, 200)
+        response_data = response.json()
+        self.assertIn("totalTime", response_data)
+
+    def test_get_average_online_time(self):
+        client = TestClient(app)
+        user_id = "df60f3f8-0e13-b64a-76b5-863bae54478e"
+        response = client.get(f"/api/stats/user/average?user_id={user_id}")
+        self.assertEqual(response.status_code, 200)
+        response_data = response.json()
+        self.assertIn("weeklyAverage", response_data)
+        self.assertIn("dailyAverage", response_data)
+
+    def test_get_report(self):
+        client = TestClient(app)
+        report_name = "example_report"
+        from_date = "2023-10-01"
+        to_date = "2023-10-31"
+        response = client.get(f"/api/report/{report_name}?from_date={from_date}&to_date={to_date}")
+        self.assertEqual(response.status_code, 200)
+        response_data = response.json()
+
+if __name__ == '__main__':
+    unittest.main()
